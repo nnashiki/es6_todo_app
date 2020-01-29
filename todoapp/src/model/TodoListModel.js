759ec47b3b28fd15764029ed731df7104d2a7e78
nnashiki <n.nashiki.work@gmail.com>
2020-01-30 00:57:39 +0900

    fix

--- todoapp/src/model/TodoListModel.js
+++ todoapp/src/model/TodoListModel.js
@@ -1,25 +1,88 @@
-import { element } from "./html-util.js";
-import { TodoItemView } from "./TodoItemView.js";
+import { EventEmitter } from "../EventEmitter.js";
 
-export class TodoListView {
+export class TodoListModel extends EventEmitter {
     /**
-     * `todoItems`に対応するTodoリストのHTML要素を作成して返す
-     * @param {TodoItemModel[]} todoItems TodoItemModelの配列
-     * @param {function({id:string, completed: boolean})} onUpdateTodo チェックボックスの更新イベントリスナー
-     * @param {function({id:string})} onDeleteTodo 削除ボタンのクリックイベントリスナー
-     * @returns {Element} TodoItemModelの配列に対応したリストのHTML要素
+     * @param {TodoItemModel[]} [items] 初期アイテム一覧（デフォルトは空の配列）
      */
-    createElement(todoItems, { onUpdateTodo, onDeleteTodo }) {
-        const todoListElement = element`<ul />`;
-        // 各TodoItemモデルに対応したHTML要素を作成し、リスト要素へ追加する
-        todoItems.forEach(todoItem => {
-            const todoItemView = new TodoItemView();
-            const todoItemElement = todoItemView.createElement(todoItem, {
-                onDeleteTodo,
-                onUpdateTodo
-            });
-            todoListElement.appendChild(todoItemElement);
+    constructor(items = []) {
+        super();
+        this.items = items;
+    }
+
+    /**
+     * TodoItemの合計個数を返す
+     * @returns {number}
+     */
+    getTotalCount() {
+        return this.items.length;
+    }
+
+    /**
+     * 表示できるTodoItemの配列を返す
+     * @returns {TodoItemModel[]}
+     */
+    getTodoItems() {
+        return this.items;
+    }
+
+    /**
+     * TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
+     * @param {Function} listener
+     */
+    onChange(listener) {
+        this.addEventListener("change", listener);
+    }
+
+    /**
+     * TodoListの状態が更新されたときに呼び出されるリスナー関数を解除する
+     * @param {Function} listener
+     */
+    offChange(listener) {
+        this.removeEventListener("change", listener);
+    }
+
+    /**
+     * 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
+     */
+    emitChange() {
+        this.emit("change");
+    }
+
+    /**
+     * TodoItemを追加する
+     * @param {TodoItemModel} todoItem
+     */
+    addTodo(todoItem) {
+        // タイトルが空のものは追加しない
+        if (todoItem.isEmptyTitle()) {
+            return;
+        }
+        this.items.push(todoItem);
+        this.emitChange();
+    }
+
+    /**
+     * 指定したidのTodoItemのcompletedを更新する
+     * @param {{ id:number, completed: boolean }}
+     */
+    updateTodo({ id, completed }) {
+        const todoItem = this.items.find(todo => todo.id === id);
+        if (!todoItem) {
+            return;
+        }
+        todoItem.completed = completed;
+        this.emitChange();
+    }
+
+    /**
+     * 指定したidのTodoItemを削除する
+     * @param {{ id: number }}
+     */
+    deleteTodo({ id }) {
+        // `id`に一致しないTodoItemだけを残すことで、`id`に一致するTodoItemを削除する
+        this.items = this.items.filter(todo => {
+            return todo.id !== id;
         });
-        return todoListElement;
+        this.emitChange();
     }
 }
